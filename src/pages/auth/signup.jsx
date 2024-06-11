b2f54573d0f441d6840e5c938831a527856fbbe0
progchris00 <prog.christian07@gmail.com>
2024-06-11 13:35:47 +0800

    refractor: create different class name for same function

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -5,13 +5,13 @@ function SignUpPage() {
   return (
     <main className="overlay">
       <div className="sign-up container">
-        <div className="logo-title">
+        <div className="logo-and-heading">
           <img src={logoname} alt="" id="form-logo" />
           <h1 id="form-heading">Create an account</h1>
         </div>
         <form id="sign-up-form">
-          <fieldset className="no-border">
-            <div className="left-container">
+          <fieldset className="no-border flex-center">
+            <div className="sign-up-form-left">
               <label htmlFor="profile-picture">
                 Profile Picture
                 <input type="text" id="profile-picture" required />
@@ -29,7 +29,7 @@ function SignUpPage() {
                 <input type="text" id="branch" required />
               </label>
             </div>
-            <div className="right-container">
+            <div className="sign-up-form-right">
               <label htmlFor="phone-number">
                 Phone no.
                 <input type="tel" id="phone-number" required />
@@ -46,13 +46,15 @@ function SignUpPage() {
                 Confirm Password
                 <input type="password" id="confirm-password" required />
               </label>
-              <input
-                type="submit"
-                className="submit-button"
-                value={"Register"}
-              ></input>
-              <div className="login-page-options">
-                <Link to="/auth/login">Already have an account?</Link>
+              <div>
+                <input
+                  type="submit"
+                  className="submit-button"
+                  value={"Register"}
+                ></input>
+                <Link to="/auth/login" className="link">
+                  Already have an account?
+                </Link>
               </div>
             </div>
           </fieldset>
