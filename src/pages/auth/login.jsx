a271e69eb8d62a8392ed51d0b1356c37d2eaa4a5
progchris00 <prog.christian07@gmail.com>
2024-06-13 09:35:09 +0800

    feature: responsive form container and input fiels on login page
    Changed fixed sizes to percents

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -4,29 +4,31 @@ import { Link } from "react-router-dom";
 function LoginPage() {
   return (
     <main className="overlay">
-      <div className="login container flex-center-column">
-        <img src={FormLogo} alt="" id="form-logo" />
-        <h1 id="form-heading">Login</h1>
-        <form id="login-form">
-          <fieldset className="form-fieldset no-border flex-center-column gap">
-            <label htmlFor="email-or-phone">
-              Email or Phone
-              <input type="text" id="email-or-phone" required />
-            </label>
-            <label htmlFor="password">
-              Password
-              <input type="password" id="password" required />
-            </label>
-            <input
-              type="submit"
-              className="submit-button"
-              value={"Log In"}
-            ></input>
-          </fieldset>
-        </form>
-        <div className="flex-space-between">
-          <Link to="/auth/sign-up">Create an account</Link>
-          <Link to="/auth/forgot-password">Forgot Password?</Link>
+      <div className="form-container">
+        <div className="login container flex-center-column">
+          <img src={FormLogo} alt="" id="form-logo" />
+          <h1 id="form-heading">Login</h1>
+          <form id="login-form">
+            <fieldset className="form-fieldset no-border flex-center-column gap">
+              <label htmlFor="email-or-phone">
+                Email or Phone
+                <input type="text" id="email-or-phone" required />
+              </label>
+              <label htmlFor="password">
+                Password
+                <input type="password" id="password" required />
+              </label>
+              <input
+                type="submit"
+                className="submit-button"
+                value={"Log In"}
+              ></input>
+            </fieldset>
+          </form>
+          <div className="flex-space-between">
+            <Link to="/auth/sign-up">Create an account</Link>
+            <Link to="/auth/forgot-password">Forgot Password?</Link>
+          </div>
         </div>
       </div>
     </main>
