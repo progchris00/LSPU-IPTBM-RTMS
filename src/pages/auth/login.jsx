7548353881afe5dfbcf20df9b4e1284be3c9d541
progchris00 <prog.christian07@gmail.com>
2024-06-16 07:10:05 +0800

    add: adjust position and size of reset password form

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -25,7 +25,7 @@ function LoginPage() {
               ></input>
             </fieldset>
           </form>
-          <div className="flex-space-between login-option">
+          <div className="flex-space-between">
             <Link to="/auth/sign-up">Create an account</Link>
             <Link to="/auth/forgot-password">Forgot Password?</Link>
           </div>
--- src/pages/auth/reset-password.jsx
+++ src/pages/auth/reset-password.jsx
@@ -1,28 +1,34 @@
 import FormLogo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 function ResetPasswordPage() {
   return (
     <main className="overlay">
-      <div className="reset container flex-center-column">
-        <img src={FormLogo} alt="" id="form-logo" />
-        <h1 id="form-heading">Reset Password</h1>
-        <form id="reset-password-form">
-          <fieldset className="form-fieldset no-border flex-center-column gap">
-            <label htmlFor="email-or-phone">
-              Password
-              <input type="text" id="email-or-phone" required />
-            </label>
-            <label htmlFor="email-or-phone">
-              Confirm Password
-              <input type="text" id="email-or-phone" required />
-            </label>
-            <input
-              type="submit"
-              className="submit-button"
-              value={"Reset"}
-            ></input>
-          </fieldset>
-        </form>
+      <div className="form-container">
+        <div className="reset container flex-center-column">
+          <img src={FormLogo} alt="" id="form-logo" />
+          <h1 id="form-heading">Reset Password</h1>
+          <form id="reset-password-form">
+            <fieldset className="form-fieldset no-border flex-center-column gap">
+              <label htmlFor="password">
+                Password
+                <input type="password" id="password" required autoFocus />
+              </label>
+              <label htmlFor="confirm-password">
+                Confirm Password
+                <input type="password" id="confirm-password" required />
+              </label>
+              <input
+                type="submit"
+                className="submit-button"
+                value={"Reset"}
+              ></input>
+            </fieldset>
+            <div>
+              <Link to="/auth/login">Log in</Link>
+            </div>
+          </form>
+        </div>
       </div>
     </main>
   );
