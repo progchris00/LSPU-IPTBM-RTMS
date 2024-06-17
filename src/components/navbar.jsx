4a21e029c361130e986cbbdec24c10b860238297
progchris00 <prog.christian07@gmail.com>
2024-06-17 20:45:55 +0800

    add: wrap nav in div to center

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,32 +6,34 @@ import Logo from "../assets/images/logo.png";
 function Navbar() {
   return (
     <header>
-      <img src={Logo} alt="LSPU IPTBM Logo" id="form-logo" />
-      <h1 className="nav-logoname">LSPU IPTBM</h1>
-      <nav>
-        <ul className="nav-menu">
-          <li>
-            <Link to="/Home">Home</Link>
-          </li>
-          <li>
-            <Link to="/About-us">About us</Link>
-          </li>
-          <li>
-            <Link to="/Technologies">Technologies</Link>
-          </li>
-          <li>
-            <Link to="/Contact-us">Contact us</Link>
-          </li>
-        </ul>
-        <div className="auth-buttons">
-          <Link to="/auth/login" className="ghost-button">
-            Login
-          </Link>
-          <Link to="/auth/sign-up" className="solid-button">
-            Sign Up
-          </Link>
-        </div>
-      </nav>
+      <div className="nav-wrapper">
+        <img src={Logo} alt="LSPU IPTBM Logo" id="form-logo" />
+        <h1 className="nav-logoname">LSPU IPTBM</h1>
+        <nav>
+          <ul className="nav-menu">
+            <li>
+              <Link to="/Home">Home</Link>
+            </li>
+            <li>
+              <Link to="/About-us">About us</Link>
+            </li>
+            <li>
+              <Link to="/Technologies">Technologies</Link>
+            </li>
+            <li>
+              <Link to="/Contact-us">Contact us</Link>
+            </li>
+          </ul>
+          <div className="auth-buttons">
+            <Link to="/auth/login" className="ghost-button">
+              Login
+            </Link>
+            <Link to="/auth/sign-up" className="solid-button">
+              Sign Up
+            </Link>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
