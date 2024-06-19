53eb252265d552b43590e5bce48b93708fdd6eaa
progchris00 <prog.christian07@gmail.com>
2024-06-19 11:21:20 +0800

    add: create hero component and add texts

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+function Hero() {
+  return (
+    <section id="hero">
+      <div className="hero-left">
+        <div>LSPU IPTBM Real Time Monitoring System</div>
+        <h2>Intellectual Property and Technology Business Management Office</h2>
+        <p>
+          Protects an organization's IP assets and technology initiatives,
+          facilitating innovation, technology transfer, and compliance with
+          relevant laws.
+        </p>
+        <button className="solid-button">Get Started</button>
+        <button className="ghost-button">Schedule a meeting</button>
+      </div>
+      <div className="hero-right"></div>
+    </section>
+  );
+}
+
+export default Hero;
