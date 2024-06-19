ddfe2730d75e2fd18df63d0211f9ce456d44b5e0
progchris00 <prog.christian07@gmail.com>
2024-06-19 11:55:00 +0800

    add: seperate hero left and right section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,24 @@
+import { Link } from "react-router-dom";
+
 function Hero() {
   return (
     <section id="hero">
-      <div className="hero-left">
-        <div>LSPU IPTBM Real Time Monitoring System</div>
-        <h2>Intellectual Property and Technology Business Management Office</h2>
-        <p>
-          Protects an organization's IP assets and technology initiatives,
-          facilitating innovation, technology transfer, and compliance with
-          relevant laws.
-        </p>
-        <button className="solid-button">Get Started</button>
-        <button className="ghost-button">Schedule a meeting</button>
+      <div className="hero-wrapper">
+        <div className="hero-left">
+          <div>LSPU IPTBM Real Time Monitoring System</div>
+          <h2>
+            Intellectual Property and Technology Business Management Office
+          </h2>
+          <p>
+            Protects an organization's IP assets and technology initiatives,
+            facilitating innovation, technology transfer, and compliance with
+            relevant laws.
+          </p>
+          <Link className="solid-button">Get Started</Link>
+          <Link className="ghost-button">Schedule a meeting</Link>
+        </div>
+        <div className="hero-right">img</div>
       </div>
-      <div className="hero-right"></div>
     </section>
   );
 }
