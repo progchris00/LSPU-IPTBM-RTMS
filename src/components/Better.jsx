21309687affdd8a9fde4e16c7673a7f7b038218f
progchris00 <prog.christian07@gmail.com>
2024-06-23 16:40:58 +0800

    feat: adjust better section font spacing on mobile version

--- src/components/Better.jsx
+++ src/components/Better.jsx
@@ -5,18 +5,20 @@ function Better() {
   return (
     <section id="better">
       <div className="wrapper">
-        <div className="better left">
-          <h3 className="better-title">
-            How <span className="blue-text">LSPU IPTBM Work to Serve </span>You
-            Better
-          </h3>
-        </div>
-        <div className="better right">
-          <p className="better description">
-            We are dedicated to serving you better by offering comprehensive and
-            tailored services that ensure the effective management and
-            protection of your intellectual property.
-          </p>
+        <div className="better-texts">
+          <div className="better left">
+            <h3 className="better-title">
+              How <span className="blue-text">LSPU IPTBM Work to Serve </span>
+              You Better
+            </h3>
+          </div>
+          <div className="better right">
+            <p className="better description">
+              We are dedicated to serving you better by offering comprehensive
+              and tailored services that ensure the effective management and
+              protection of your intellectual property.
+            </p>
+          </div>
         </div>
         <div className="better-images">
           <img src={ImageOne} className="better-image-one" alt="" />
