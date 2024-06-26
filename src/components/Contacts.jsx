c3ab2723866f9844919283f31bbbaf58f179528f
progchris00 <prog.christian07@gmail.com>
2024-06-26 09:58:21 +0800

    feat: add and style left side of contact section

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,26 +1,30 @@
+import AddressLogo from "../assets/images/location.svg";
+import PhoneLogo from "../assets/images/phone.svg";
+import EmailLogo from "../assets/images/email.svg";
+
 function Contacts() {
   return (
     <section id="contacts">
       <div className="wrapper">
         <div className="contact left">
           <div className="text-wrapper">
-            <h3>Get in touch with us</h3>
-            <p>
+            <h3 className="section-title">Get in touch with us</h3>
+            <p className="description">
               We love to hear from you. Our teams are always here to answer your
               message.
             </p>
           </div>
           <div className="contact-information">
-            <span>
-              <img src="" alt="" />
+            <span className="contact-info">
+              <img src={AddressLogo} alt="" />
               <p id="contact-address">123 Street, City Country</p>
             </span>
-            <span>
-              <img src="" alt="" />
+            <span className="contact-info">
+              <img src={PhoneLogo} alt="" />
               <p id="contact-number">+234 567890</p>
             </span>
-            <span>
-              <img src="" alt="" />
+            <span className="contact-info">
+              <img src={EmailLogo} alt="" />
               <p id="contact-email">mail@gmail.com</p>
             </span>
           </div>
